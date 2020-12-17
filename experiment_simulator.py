1f773be9ce87522c2558945342ee6101144fb046
Ubuntu <ubuntu@grvm.novalocal>
2020-12-17 00:31:01 +0000

    model changed

--- experiment_simulator.py
+++ experiment_simulator.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 # input domain name from command line
 domain = sys.argv[1]
+scenarios = sys.argv[2]
 
 ema_logging.LOG_FORMAT = '[%(name)s/%(levelname)s/%(processName)s] %(message)s'
 ema_logging.log_to_stderr(ema_logging.INFO)
@@ -50,8 +51,8 @@ model.outcomes = [ScalarOutcome('p_10'),
 
 
 from ema_workbench import save_results
-results = perform_experiments(model, 1000)
-save_results(results, '1000_scenarios_%s.tar.gz'%domain)
+#results = perform_experiments(model, 1000)
+#save_results(results, '1000_scenarios_%s.tar.gz'%domain)
 
-sa_results = perform_experiments(model, scenarios=1050, uncertainty_sampling='sobol')
-save_results(sa_results, '1050_scenarios_%s_sobol.tar.gz'%domain)
+sa_results = perform_experiments(model, scenarios=int(scenarios), uncertainty_sampling='sobol')
+save_results(sa_results, '%s_scenarios_%s_sobol.tar.gz'%(scenarios, domain))
